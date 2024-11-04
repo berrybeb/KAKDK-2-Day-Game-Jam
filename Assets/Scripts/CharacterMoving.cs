6b3ce5bbd054dd5fe38564a049d4dcf1f6ae280b
BjoernTroldahl <72730043+BjoernTroldahl@users.noreply.github.com>
2024-11-04 21:37:22 +0100

    Can make the character dissappear if you click REJECT now

--- Assets/Scripts/CharacterMoving.cs
+++ Assets/Scripts/CharacterMoving.cs
@@ -1,19 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ForwardMovingCharacter : MonoBehaviour
 { 
     // Speed of movement along the x-axis
     public float speed = 2.0f;
-
     // The target x-coordinate at which the sprite should stop
     private float stopPositionX;
-
+    //public GameObject RejectButton;
     void Start()
     {
-        // Calculate 1/3 of the game window width in world units
-        stopPositionX = Camera.main.ViewportToWorldPoint(new Vector3(1.0f / 2.0f, 0, 0)).x;
+    // Calculate 1/3 of the game window width in world units
+    stopPositionX = Camera.main.ViewportToWorldPoint(new Vector3(1.0f / 2.0f, 0, 0)).x;
     }
 
     void Update()
--- Assets/Scripts/REJECT_ONclick.cs
+++ Assets/Scripts/REJECT_ONclick.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Accept_ONclick : MonoBehaviour
+public class Reject_ONclick : MonoBehaviour
 {
+    public GameObject Character;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,10 @@ public class Accept_ONclick : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void Click(){
+    Character.SetActive(false); 
     }
 }
